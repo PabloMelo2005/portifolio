2dca76a1d9fb2af778d83aa842481ae5308b7597
PabloMelo2005 <pablobertoldo.melo@gmail.com>
2024-08-28 10:15:04 -0300

    AtividadesJava
    Atividades de sistemas distribuidos e mobile

---
+++ Java_Faculdade/SistemasDistribuidos/Ativ07_SistemasDistribuidos/Livro.java
@@ -0,0 +1,10 @@
+package atividade7;
+
+public class Livro {
+
+	public static void main(String[] args) {
+		LojaDeLivros Livro = new LojaDeLivros("Dom Casmurro", "Machado de Assis", 1899, 29.90);
+		Livro.imprimirDados();
+	}
+
+}
