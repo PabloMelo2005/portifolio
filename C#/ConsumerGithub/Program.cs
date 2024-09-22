b95ee346838ddcf70a0a780ec9b00236c61241f1
PabloMelo2005 <pablobertoldo.melo@gmail.com>
2024-09-22 20:42:07 -0300

    Consumer GitHub

---
+++ C#/ConsumerGithub/Program.cs
@@ -0,0 +1,41 @@
+﻿﻿using static System.Console;
+using System.Net.Http;
+using System.Text.Json;
+
+WriteLine("Digite o ID do usuário: "); 
+var id = ReadLine(); 
+
+var url = $@"https://api.github.com/user/{id}"; 
+
+WriteLine($"Realizando a requisição para {url}...");
+
+var cliente = new HttpClient(); 
+cliente.DefaultRequestHeaders.Add("User-Agent", "IntegracaoViacep");
+
+try {
+    HttpResponseMessage? response = await cliente.GetAsync(url); 
+    response.EnsureSuccessStatusCode(); 
+
+    string respostaAPI = await response.Content.ReadAsStringAsync();
+
+    /**Transformação do JSON em um objeto, podendo acessar seus atributos: */
+    var usuario = JsonSerializer.Deserialize<GithubUser>(respostaAPI, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+    WriteLine("ID do usuario: " + usuario.id);
+    WriteLine("Nome: " + usuario.name);
+    WriteLine("Empresa " + usuario.company);
+    WriteLine("Localização: " + usuario.location);
+    WriteLine("Login: " + usuario.login);
+
+}
+
+catch (HttpRequestException ex) /**Exceção da requisição web */
+{
+    Console.WriteLine($"Ocorreu um erro ao fazer a requisição para {url}:");
+    Console.WriteLine($"Erro: {ex.Message}");
+}
+catch (Exception ex) /**Exceção do código: */
+{
+    Console.WriteLine("Ocorreu um erro inesperado:");
+    Console.WriteLine($"Erro: {ex.Message}");
+}
