1b318747caa0422ac8a1354cd003fa4fe80e6a96
Pablo Rodrigues Bertoldo Melo - 32327220 <32327220@ulife.com.br>
2024-10-25 10:08:35 -0300

    AtividadePizzaStore + SQL

--- C#/PizzaStoreApi/obj/Debug/net8.0/PizzaStoreApi.AssemblyInfo.cs
+++ C#/PizzaStoreApi/obj/Debug/net8.0/PizzaStoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c0f923a27c66aab77b882c76a25fb0e4029ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18415cffe0bc1ed905ca349587b753bac5d2a294")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
